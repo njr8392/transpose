e05211fff73db113a6c64fb046b2e468dcf1b341
Nicholas Rodgers <nrodgers88@gmail.com>
2023-03-28 22:48:36 -0400

    dumb program that reads blocks of data across many columns and combines them iinto one block of data.

---
+++ csv-agg.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
+
+/*the goal is to read a bunch of tables from a csv file and write them to a single table.
+adobe report builder forces us to put data in blocks even if we eventually need to combine
+all the data. this will read each all blocks and combine them into one block. */
+var WRITER *os.File = os.Stdout
+
+//number of items within each block
+const STEPSIZE = 4
+
+func main() {
+	file := readcsv(os.Args[1])
+	cleansed := flatten(file)
+	fmt.Println(cleansed)
+	splt := split(cleansed, STEPSIZE)
+	writer := csv.NewWriter(WRITER)
+	err := writer.WriteAll(splt)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
+
+//split the line into equal chunks. Done after removing whitespace
+func split(buf [][]string, step int) [][]string {
+	var cleaned [][]string
+	var tmp []string
+	for _, line := range buf {
+		for i := 0; i < len(line); i++ {
+			tmp = append(tmp, line[i])
+
+			if len(tmp) == STEPSIZE {
+				cleaned = append(cleaned, tmp)
+				tmp = nil
+			}
+		}
+	}
+	return cleaned
+}
+
+func flatten(data [][]string) [][]string {
+	for i, line := range data {
+		var clean []string
+		for _, item := range line {
+			if item != "" {
+				clean = append(clean, item)
+			}
+		}
+		data[i] = clean
+	}
+	return data
+}
+
+func readcsv(file string) [][]string {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil
+	}
+	defer f.Close()
+	reader := csv.NewReader(f)
+	data, err := reader.ReadAll()
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	return data
+}
+
+func readCsv(file string) []byte {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil
+	}
+	defer f.Close()
+	info, _ := f.Stat()
+	data := make([]byte, info.Size())
+	f.Read(data)
+	return data
+}
