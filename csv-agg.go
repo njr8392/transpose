1e492ad629b10015026438746965cf4b2e6818bd
Nicholas Rodgers <nrodgers88@gmail.com>
2023-05-01 18:28:39 -0400

    cleaned loop in the cleaned function to make it easier to read and fixed typos in comments

--- csv-agg.go
+++ csv-agg.go
@@ -8,16 +8,15 @@ import (
 
 /*the goal is to read a bunch of tables from a csv file and write them to a single table.
 adobe report builder forces us to put data in blocks even if we eventually need to combine
-all the data. this will read each all blocks and combine them into one block. */
+all the data. this will read every block and combine them into one block. */
 var WRITER *os.File = os.Stdout
 
 //number of columns within each block
-const STEPSIZE = 4
+const STEPSIZE = 6
 
 func main() {
 	file := readcsv(os.Args[1])
 	cleansed := flatten(file)
-	fmt.Println(cleansed)
 	splt := split(cleansed, STEPSIZE)
 	writer := csv.NewWriter(WRITER)
 	err := writer.WriteAll(splt)
@@ -27,13 +26,14 @@ func main() {
 
 }
 
-//split the line into equal chunks. Done after removing whitespace
+//split the line into equal chunks based on the length of each block
+//Done after removing whitespace
 func split(buf [][]string, step int) [][]string {
 	var cleaned [][]string
 	var tmp []string
 	for _, line := range buf {
-		for i := 0; i < len(line); i++ {
-			tmp = append(tmp, line[i])
+		for cell := range line {
+			tmp = append(tmp, line[cell])
 
 			if len(tmp) == STEPSIZE {
 				cleaned = append(cleaned, tmp)
@@ -44,17 +44,19 @@ func split(buf [][]string, step int) [][]string {
 	return cleaned
 }
 
+//remove empty cells from the file
 func flatten(data [][]string) [][]string {
-	for i, line := range data {
+	var flattened [][]string
+	for _, line := range data {
 		var clean []string
 		for _, item := range line {
 			if item != "" {
 				clean = append(clean, item)
 			}
 		}
-		data[i] = clean
+		flattened = append(flattened, clean)
 	}
-	return data
+	return flattened
 }
 
 func readcsv(file string) [][]string {
